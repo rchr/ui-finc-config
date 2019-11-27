e3e84eb99ca42abc22acf9bcc71e0186b5f82dfc
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2019-11-27 14:04:56 +0100

    UIFC-102-further-bigtests add navigation tests

--- test/bigtest/interactors/source.js
+++ test/bigtest/interactors/source.js
@@ -6,12 +6,15 @@ import {
   clickable
 } from '@bigtest/interactor';
 
+import NavigationInteractor from './navigation';
+
 export default @interactor class SourceInteractor {
   static defaultScope = '[data-test-sources]';
 
   clickActiveSOURCEsCheckbox = clickable('#clickable-filter-status-active');
   instances = collection('[role=group] div a');
   instance = scoped('#pane-sourcedetails');
+  navigation = new NavigationInteractor();
 
   isLoaded = isPresent('#pane-sourceresults');
   whenLoaded() {
