980231faa191268fb1099a0053d36682082b1ebc
elsenhans <48911833+elsenhans@users.noreply.github.com>
2020-08-07 11:19:44 +0200

    UIFC-200-validate-url (#179)
    Co-authored-by: Richard Redweik <rchr@users.noreply.github.com>

--- src/components/DisplayUtils/Validate.js
+++ src/components/DisplayUtils/Validate.js
@@ -9,7 +9,7 @@ const ValidateUrl = value => {
   // http required:
   const urlRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,})/;
 
-  if (value && urlRegex.test(value)) return undefined;
+  if (!value || urlRegex.test(value)) return undefined;
   return 'Valid Url with http:// or https:// required!';
 };
 
