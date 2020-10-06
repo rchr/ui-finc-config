1ffc1abcd003d3fdab22541d971164c8fe5e13ff
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-10-06 09:44:03 +0200

    translateFilterValues

--- src/components/MetadataSources/filterConfigData.js
+++ src/components/MetadataSources/filterConfigData.js
@@ -1,14 +1,17 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
 const filterConfig = [
   {
     label: 'Implementation Status',
     name: 'status',
     cql: 'status',
     values: [
-      { name: 'Active', cql: 'active' },
-      { name: 'Request', cql: 'request' },
-      { name: 'Implementation', cql: 'implementation' },
-      { name: 'Closed', cql: 'closed' },
-      { name: 'Impossible', cql: 'impossible' }
+      { name: <FormattedMessage id="ui-finc-config.source.active" />, cql: 'active' },
+      { name: <FormattedMessage id="ui-finc-config.source.request" />, cql: 'request' },
+      { name: <FormattedMessage id="ui-finc-config.source.implementation" />, cql: 'implementation' },
+      { name: <FormattedMessage id="ui-finc-config.source.closed" />, cql: 'closed' },
+      { name: <FormattedMessage id="ui-finc-config.source.impossible" />, cql: 'impossible' }
     ],
   },
   {
