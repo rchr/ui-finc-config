90eccd927055111438ae618803bd4bbcc83b68d9
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2021-01-27 15:11:48 +0100

    UIFC-228-replace-bigtest-by-rtl-jest jest setup, add first test

---
+++ src/components/MetadataCollections/CollectionInfo/CollectionInfoView.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+
+import '../../../../test/jest/__mock__';
+import renderWithIntl from '../../../../test/jest/helpers';
+import CollectionInfoView from './CollectionInfoView';
+
+const COLLECTION = {
+  id: 'ccdbb4c7-9d58-4b59-96ef-7074c34e901b',
+  label: 'Test collection 1',
+  description: 'desc',
+};
+
+const renderSourceView = (metadataCollection = COLLECTION) => (
+  renderWithIntl(
+    <Router>
+      <CollectionInfoView id="collectionInfo" metadataCollection={metadataCollection} />
+    </Router>
+  )
+);
+
+describe('CollectionInfoView component', () => {
+  it('should display description', () => {
+    renderSourceView(COLLECTION);
+    expect(screen.getByText('desc')).toBeInTheDocument();
+  });
+  it('should display id', () => {
+    renderSourceView(COLLECTION);
+    expect(document.querySelector('#collectionInfo')).toBeInTheDocument();
+  });
+});
+
